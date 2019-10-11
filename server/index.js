ee6e642572dc4db024696700fabf071afddfdf6c
Austin Jang <austinhjang@gmail.com>
2019-10-11 15:15:35 -0700

    Build basic file structure and files. Work on seeding-database script.

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.listen(port, ()=>{
+  console.log('listening on port: ' + port);
+});
