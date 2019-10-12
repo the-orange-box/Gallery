054753a0bd9b19457513ac5b5b3c3b86a821177e
Austin Jang <austinhjang@gmail.com>
2019-10-11 17:14:02 -0700

    Add get route, change database name

--- server/database/seed-database.js
+++ server/database/seed-database.js
@@ -12,7 +12,7 @@ var seedDatabase = function (amount) {
       if (err) {
         console.log(err);
       } else {
-        console.log(result);
+        // console.log('image inserted');
       }
     }));
   }
