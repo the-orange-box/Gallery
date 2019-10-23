1663d594533bc3ff4b6c6676c8300cd1a4093a8f
Austin Jang <austinhjang@gmail.com>
2019-10-23 11:13:10 -0700

    Change server port to 3001 and client root id

--- server/index.js
+++ server/index.js
@@ -5,15 +5,21 @@ const path = require('path');
 const port = 3001;
 const cors = require('cors');
 
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
 
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/:placeid', express.static(path.resolve(__dirname, '../public')));
 app.get('/gallery', imageController.getImages);
 app.get('/gallery/:placeid', imageController.getImage);
-app.get('/*', (req, res, next)=>{
-  res.sendFile(path.resolve(__dirname, '../public/index.html'));
-});
+// app.get('/*', (req, res, next)=>{
+//   res.sendFile(path.resolve(__dirname, '../public/index.html'));
+// });
 
 app.listen(port, ()=>{
   console.log('listening on port: ' + port);
